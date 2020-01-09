b40702c3ea3f2bfe60eb534961e123c1a0a07c39
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-09 18:26:24 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-  self.end_with?(".") 
+  self.end_with?(".")
 
   end
 
@@ -12,7 +12,7 @@ class String
   end
 
   def exclamation?
-
+    self.end_with?("!") 
   end
 
   def count_sentences
