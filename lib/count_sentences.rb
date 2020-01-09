44f48caaa6d9e03f77d3dc1c88c8fa8e73e0d6c6
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-09 18:33:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".")
+
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sent_arr = self.split(/[.!?]/)
+    sent_arr.delete_if{|sentence| sentence.empty?}
+    sent_arr.length
   end
 end
